4e2de7be6acdd434303d15b163606491fa723908
Md. Jabed Hossain <jabed15578@gmail.com>
2021-12-29 14:54:18 +0600

    Datatype and Variable

---
+++ Basic C++_Syntax/Variables.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int id = 79;
+  double cgpa = 3.75;
+  char grade = 'A';
+  string first_name = "Afzal";
+  string last_name = "Jabed";
+  cout << "Hello World";
+}
