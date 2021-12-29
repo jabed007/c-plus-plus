a25c202041a0fef5d39f5c3f6e1842fc8b0f04de
Md. Jabed Hossain <jabed15578@gmail.com>
2021-12-29 15:20:43 +0600

    Input and Output in C++

---
+++ Basic C++_Syntax/Input_Output.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int id = 79;
+  double cgpa = 3.75;
+  char grade = 'A';
+  string first_name = "Afzal";
+  string last_name = "Jabed";
+  cout << "First Name: " << first_name << "\n";
+  cout << "Last Name: " << last_name << "\n";
+
+  cout << "Enter your id: "
+       << "\n";
+  cin >> id;
+  cout << "Your id is: " << id << "\n";
+
+  return 0;
+}
